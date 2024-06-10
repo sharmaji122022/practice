0017a5e193e845a076947435a6751ba279cb0313
Aditya Sharma <sharmaji742595@gmail.com>
2024-06-10 16:25:32 +0530

    my clg project

---
+++ html/intc/header.php
@@ -0,0 +1,18 @@
+<H2>BCA NOTES</H2>
+        
+        <nav class="navbar">
+            <a href="#homepage">
+                <li>HOME PAGE</li>
+            </a>
+            <a href="notes.html">
+                <li>NOTES</li>
+            </a>
+            <a href="login.html">
+                <li>PAPERS</li>
+            </a>
+            <a href="aboutus.html">
+                <li>ABOUT US</li>
+            </a>
+
+        </nav>
+  
